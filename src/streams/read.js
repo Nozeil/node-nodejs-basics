fbff910925a14b5393fa6ce98c7be43244524426
Artur Voloshin <nozeilwrk@gmail.com>
2023-06-18 19:05:01 +0300

    feat: complete streams read task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import { createReadStream } from "fs";
+import { getDirname } from "../utils/index.js";
+import { resolve } from "path";
+import { pipeline } from "stream/promises";
+
 const read = async () => {
-    // Write your code here 
+  const __dirname = getDirname(import.meta.url);
+  const path = resolve(__dirname, "files/fileToRead.txt");
+
+  try {
+    await pipeline(createReadStream(path), process.stdout);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await read();
