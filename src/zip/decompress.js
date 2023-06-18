921a4429c67e9cb93df008b44cc37364c6680673
Artur Voloshin <nozeilwrk@gmail.com>
2023-06-18 21:02:01 +0300

    feat: complete zlib decompress task

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import { createReadStream, createWriteStream } from "fs";
+import { resolve } from "path";
+import { pipeline } from "stream/promises";
+import { createUnzip } from "zlib";
+import { getDirname } from "../utils/index.js";
+
 const decompress = async () => {
-    // Write your code here 
+  const __dirname = getDirname(import.meta.url);
+
+  const filePath = resolve(__dirname, "files/fileToCompress.txt");
+  const archivePath = resolve(__dirname, "files/archive.gz");
+
+  const rs = createReadStream(archivePath);
+  const ws = createWriteStream(filePath);
+
+  try {
+    await pipeline(rs, createUnzip(), ws);
+    console.log("File decompressed");
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await decompress();
