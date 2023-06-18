464a62670c5c37cc92f1110fbb67c6b7c06d5df4
Artur Voloshin <nozeilwrk@gmail.com>
2023-06-18 20:12:50 +0300

    feat: complete streams transform task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,26 @@
+import { stdin, stdout } from "process";
+import { Transform } from "stream";
+import { pipeline } from "stream/promises";
+
+const reverse = new Transform({
+  transform(chunk) {
+    let result = "";
+    const stringifiedChunk = chunk.toString().trim();
+
+    for (let i = stringifiedChunk.length - 1; i >= 0; i--) {
+      result += stringifiedChunk[i];
+    }
+
+    this.push(result + "\n");
+  },
+});
+
 const transform = async () => {
-    // Write your code here 
+  try {
+    await pipeline(stdin, reverse, stdout);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await transform();
