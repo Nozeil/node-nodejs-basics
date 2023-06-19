3ba0bcb7f7d229595d307e904e47e82cf2aabf95
Artur Voloshin <nozeilwrk@gmail.com>
2023-06-19 15:30:48 +0300

    feat: complete worker threads task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,33 @@
+import { getDirname } from "../utils/index.js";
+import { resolve } from "path";
+import { Worker } from "worker_threads";
+import { cpus } from "os";
+
 const performCalculations = async () => {
-    // Write your code here
+  const __dirname = getDirname(import.meta.url);
+  const path = resolve(__dirname, "worker.js");
+
+  const threads = cpus().length;
+  const workersPool = [];
+
+  for (let i = 0, workerData = 10; i < threads; i++, workerData++) {
+    const asyncWorker = new Promise((res, rej) => {
+      const worker = new Worker(path, { workerData });
+
+      worker.on("message", res);
+      worker.on("error", rej);
+    });
+
+    workersPool.push(asyncWorker);
+  }
+
+  const result = (await Promise.allSettled(workersPool)).map((item) =>
+    item.status === "fulfilled"
+      ? { status: "resolved", data: item.value }
+      : { status: "error", data: null }
+  );
+
+  console.log(result);
 };
 
 await performCalculations();
