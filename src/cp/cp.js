84fb6881f5d5315a577f1ecb56d3ee4a3ab974cf
Artur Voloshin <nozeilwrk@gmail.com>
2023-06-19 18:28:07 +0300

    feat: complete child processes task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,22 @@
+import { resolve } from "path";
+import { getDirname } from "../utils/index.js";
+import { fork } from "child_process";
+import { stdin, stdout } from "process";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const __dirname = getDirname(import.meta.url);
+  const path = resolve(__dirname, "files/script.js");
+
+  fork(path, args);
+
+  // With silent option
+  /* 
+  const childProcess = fork(path, args, { silent: true });
+
+  childProcess.stdout.pipe(stdout);
+  stdin.pipe(childProcess.stdin); 
+  */
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(/* [someArgument1, someArgument2, ...] */);
